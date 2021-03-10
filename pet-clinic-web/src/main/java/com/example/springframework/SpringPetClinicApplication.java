e92503554cf29dc2244c7afaf32869a928d3cd81
Jesus Gascon <jgasconrueda@gmail.com>
2021-03-10 09:50:16 +0100

    Multimodule maven project. closes #1

--- pet-clinic-web/src/main/java/com/example/springframework/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/example/springframework/SpringPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.example.springframework.springpetclinic;
+package com.example.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
