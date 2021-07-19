cb85d9dbdc7266af3e8083f0a5228e9fb9934f7f
Jesus Gascon <jgasconrueda@gmail.com>
2021-07-19 18:44:09 +0200

    Added Pets and Contact info to owners on startup. closes #32

--- pet-clinic-web/src/main/java/com/example/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/springframework/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.example.springframework.bootstrap;
 
 import com.example.springframework.model.Owner;
+import com.example.springframework.model.Pet;
 import com.example.springframework.model.PetType;
 import com.example.springframework.model.Vet;
 import com.example.springframework.services.OwnerService;
@@ -8,6 +9,8 @@ import com.example.springframework.services.PetTypeService;
 import com.example.springframework.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -29,22 +32,43 @@ public class DataLoader implements CommandLineRunner {
         PetType dog = new PetType();
         dog.setName("Dog");
 
-        petTypeService.save(dog);
+        PetType savedDogPetType = petTypeService.save(dog);
 
         PetType cat = new PetType();
         cat.setName("Cat");
 
-        petTypeService.save(cat);
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Miguel");
         owner1.setLastName("Perez");
+        owner1.setAddress("Calle Cardanas 2");
+        owner1.setCity("Merida");
+        owner1.setTelephone("666999888");
+
+        Pet golfo = new Pet();
+        golfo.setPetType(savedDogPetType);
+        golfo.setOwner(owner1);
+        golfo.setBirthDate(LocalDate.now());
+        golfo.setName("Golfo");
+        owner1.getPets().add(golfo);
 
         ownerService.save(owner1);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Juan");
         owner2.setLastName("SinMiedo");
+        owner2.setAddress("Residencial Aldebaran 9");
+        owner2.setCity("Dos hermanas");
+        owner2.setTelephone("955663446");
+
+        Pet rita = new Pet();
+        rita.setPetType(savedCatPetType);
+        rita.setOwner(owner2);
+        rita.setBirthDate(LocalDate.now());
+        rita.setName("Rita");
+        owner2.getPets().add(rita);
 
         ownerService.save(owner2);
 
