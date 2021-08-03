2aa48f1c43db25442d1a3acd894b0996f0119277
Jesus Gascon <jgasconrueda@gmail.com>
2021-08-03 17:50:44 +0200

    Implement Repositories. closes #38

---
+++ pet-clinic-data/src/main/java/com/example/springframework/repositories/VetRepository.java
@@ -0,0 +1,7 @@
+package com.example.springframework.repositories;
+
+import com.example.springframework.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetRepository extends CrudRepository<Vet, Long> {
+}
