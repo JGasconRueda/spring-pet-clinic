0686d99451bd5a4b66b0852d56f4c82d2a09308b
Jesus Gascon <jgasconrueda@gmail.com>
2021-08-09 17:54:45 +0200

    Update profiles for JPA Service. closes #47

--- pet-clinic-data/src/main/java/com/example/springframework/services/map/SpecialityMapService.java
+++ pet-clinic-data/src/main/java/com/example/springframework/services/map/SpecialityMapService.java
@@ -2,11 +2,13 @@ package com.example.springframework.services.map;
 
 import com.example.springframework.model.Speciality;
 import com.example.springframework.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialtyService {
 
     @Override
--- pet-clinic-data/src/main/java/com/example/springframework/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/example/springframework/services/map/VisitMapService.java
@@ -2,10 +2,12 @@ package com.example.springframework.services.map;
 
 import com.example.springframework.model.Visit;
 import com.example.springframework.services.VisitService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
 
     @Override
