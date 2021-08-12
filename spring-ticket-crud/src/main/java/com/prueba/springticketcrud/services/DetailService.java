3c4dab6780ca7126f4c86dc9524c8b0ef806aa35
Jesús Gascón Rueda <41326822+JGasconRueda@users.noreply.github.com>
2021-08-12 23:05:12 +0200

    Delete spring-ticket-crud/src/main/java/com/prueba/springticketcrud directory

--- spring-ticket-crud/src/main/java/com/prueba/springticketcrud/services/DetailService.java
+++
@@ -1,6 +0,0 @@
-package com.prueba.springticketcrud.services;
-
-import com.prueba.springticketcrud.domain.Detail;
-
-public interface DetailService extends CrudService<Detail,Long> {
-}
--- spring-ticket-crud/src/main/java/com/prueba/springticketcrud/services/TicketService.java
+++
@@ -1,9 +0,0 @@
-package com.prueba.springticketcrud.services;
-
-import com.prueba.springticketcrud.domain.Ticket;
-
-import java.sql.Timestamp;
-
-public interface TicketService extends CrudService<Ticket, Long> {
-    Iterable<Ticket> findByRangeOfDate(Timestamp startDate, Timestamp endDate);
-}
